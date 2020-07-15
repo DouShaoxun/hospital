171844fe5e6f14ac3d3833097c7dd13ef77abe2f
DouShaoxun <dsx1046811165@gmail.com>
2020-07-15 21:34:53 +0800

    init

---
+++ src/main/java/cn/dsx/hospital/HospitalApplication.java
@@ -0,0 +1,16 @@
+package cn.dsx.hospital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Classname: HospitalApplication
+ * @Author: Dsx
+ * @Date: 2020/07/15/21:28
+ */
+@SpringBootApplication
+public class HospitalApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HospitalApplication.class, args);
+    }
+}
